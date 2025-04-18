6a8051e5d602b772927f2614e654c7c72396d6c5
allregnws <promisebassey8081@gmail.com>
2025-04-18 16:13:43 +0100

    Create kp.js

---
+++ kp.js
@@ -0,0 +1,5 @@
+var password = 'KEP-CEP'
+password=prompt('SMS :Byenveni nan pòtal rekritman nou an, Klike sou OK pou kòmanse aplikasyon w lan.  ','KEP-CEP');
+if (password != 'KEP-CEP') {
+location.href='https://allregnws.github.io/KEP.Rekritman.Potal';
+}
